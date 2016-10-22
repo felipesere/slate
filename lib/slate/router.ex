88aaddd65ffd3db939f7f0eb0618d56f3cfcc15d
Felipe Sere <felipesere@gmail.com>
2016-10-22 11:23:57 +0100

    Render the index page with all images

--- lib/slate/router.ex
+++ lib/slate/router.ex
@@ -2,14 +2,24 @@ defmodule Slate.Router do
   use Plug.Router
   use Plug.Builder
 
-  plug Plug.Static, at: "/public", from: :slate, only: ~w(main.css normalize.css)
+  plug Plug.Static, at: "/public", from: :slate, only: ~w(main.css normalize.css images)
   plug :match
   plug :dispatch
 
   get "/" do
-    response = EEx.eval_string("<html><head></head><body>Hello <%= thing %></body></html>", [thing: "World!"])
-    send_resp(conn, 200, response)
+    render_template(conn, "index")
   end
+
+  def render_template(conn, name) do
+    name
+    |> expand
+    |> template
+    |> respond(conn)
+  end
+
+  defp expand(name), do: Path.expand("lib/templates/#{name}.html.eex")
+  defp template(path), do: EEx.eval_file(path)
+  defp respond(body, conn), do: send_resp(conn, 200, body)
 
   match _ do
     send_resp(conn, 404, "oops")
