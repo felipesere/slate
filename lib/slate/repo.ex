8799f03faea94e0bdae0b1c97fdec3a43836218b
Felipe Sere <felipesere@gmail.com>
2016-10-30 21:56:51 +0000

    Extract data into an initial 'repo' module

---
+++ lib/slate/repo.ex
@@ -0,0 +1,70 @@
+defmodule Repo do
+  @images %{
+    1 => %Image{id: 1,
+      image: "london.jpg",
+      title: "London",
+      subtitle: "March 1, 2015"},
+    3 => %Image{id: 3,
+      image: "spring.jpg",
+      title: "Spring",
+      subtitle: "April 4, 2016"},
+    5 => %Image{id: 5,
+      image: "waves.jpg",
+      title: "Waves",
+      subtitle: "March 23, 2014"},
+    6 => %Image{id: 6,
+      image: "outcropping.jpg",
+      title: "Outcropping",
+      subtitle: "July 17, 2014",
+      description: "This pictures was shot very early in the morning on the other side of Madeira from where we were staying. I like how the large rock looks as if were standing calmly almost proudly over the water. The clouds help in giving the entire scene a heroic atmosphere.",
+      exif: %Exif{aperture: 11,
+                  camera: "Canon EOS 70D",
+                  focal_length: 10,
+                  iso: 100,
+                  shutter_speed: "30s"
+                }
+
+    },
+    7 => %Image{id: 7,
+      image: "beach.jpg",
+      title: "Beach",
+      subtitle: "August 29, 2015"},
+    8 => %Image{id: 8,
+      image: "rocks.jpg",
+      title: "Rocks",
+      subtitle: "August 31, 2014"},
+    2 => %Gallery{id: 2,
+      images: [8, 7, 5, 6],
+      title: "Madeira",
+      subtitle: "March 16, 2015"},
+    4 => %Gallery{id: 4,
+      images: [8, 7, 5, 6],
+      title: "Tenerife",
+      subtitle: "November 7, 2014"}
+    }
+
+    def all do
+      @images
+      |> Map.values
+      |> Enum.map(fn(%Gallery{} = g) -> expand(g)
+                    (x) -> x end)
+    end
+
+    def expand(gallery = %Gallery{images: image_ids}) do
+      %{ gallery | images: Enum.map(image_ids, fn(id) -> Map.fetch!(@images, id) end)}
+    end
+
+    def gallery(id) do
+      case Map.fetch(@images, id) do
+        {:ok, %Gallery{} = gallery} -> {:ok, expand(gallery)}
+        _ -> :not_found
+      end
+    end
+
+    def image(id) do
+      case Map.fetch(@images, id) do
+        {:ok, %Image{} = image} -> {:ok, image}
+        _ -> :not_found
+      end
+    end
+end
--- lib/slate/router.ex
+++ lib/slate/router.ex
@@ -7,73 +7,22 @@ defmodule Slate.Router do
   plug :match
   plug :dispatch
 
-  @images %{
-    1 => %Image{id: 1,
-      image: "london.jpg",
-      title: "London",
-      subtitle: "March 1, 2015"},
-    3 => %Image{id: 3,
-      image: "spring.jpg",
-      title: "Spring",
-      subtitle: "April 4, 2016"},
-    5 => %Image{id: 5,
-      image: "waves.jpg",
-      title: "Waves",
-      subtitle: "March 23, 2014"},
-    6 => %Image{id: 6,
-      image: "outcropping.jpg",
-      title: "Outcropping",
-      subtitle: "July 17, 2014",
-      description: "This pictures was shot very early in the morning on the other side of Madeira from where we were staying. I like how the large rock looks as if were standing calmly almost proudly over the water. The clouds help in giving the entire scene a heroic atmosphere.",
-      exif: %Exif{aperture: 11,
-                  camera: "Canon EOS 70D",
-                  focal_length: 10,
-                  iso: 100,
-                  shutter_speed: "30s"
-                }
-
-    },
-    7 => %Image{id: 7,
-      image: "beach.jpg",
-      title: "Beach",
-      subtitle: "August 29, 2015"},
-    8 => %Image{id: 8,
-      image: "rocks.jpg",
-      title: "Rocks",
-      subtitle: "August 31, 2014"},
-    2 => %Gallery{id: 2,
-      images: [8, 7, 5, 6],
-      title: "Madeira",
-      subtitle: "March 16, 2015"},
-    4 => %Gallery{id: 4,
-      images: [8, 7, 5, 6],
-      title: "Tenerife",
-      subtitle: "November 7, 2014"}
-    }
-
   get "/" do
-    all = Enum.map(Map.values(@images),
-                   fn(%Gallery{} = g) -> expand(g)
-                      (x) -> x end)
-
-    render_template(conn, "index", [entities: all])
-  end
-
-  def expand(gallery = %Gallery{images: image_ids}) do
-    %{ gallery | images: Enum.map(image_ids, fn(id) -> Map.fetch!(@images, id) end)}
+    render_template(conn, "index", [entities: Repo.all])
   end
 
   get "/gallery/:id" do
     {id, _} = Integer.parse(id)
-    case Map.fetch(@images, id) do
-      {:ok, gallery} -> View.within_layout("gallery", [gallery: expand(gallery)]) |> respond(conn)
+
+    case Repo.gallery(id) do
+      {:ok, gallery} -> View.within_layout("gallery", [gallery: gallery]) |> respond(conn)
       _ -> not_found(conn)
     end
   end
 
   get "/solo-image/:id" do
     {id, _} = Integer.parse(id)
-    case Map.fetch(@images, id) do
+    case Repo.image(id) do
       {:ok, image} -> View.within_layout("solo-image", [image: image]) |> respond(conn)
       _ -> not_found(conn)
     end
