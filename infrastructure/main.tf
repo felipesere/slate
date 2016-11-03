2ba77b5fe3dc6ea87e95b554d98d79cd402e6b06
Felipe Sere <felipesere@gmail.com>
2016-11-03 08:05:46 +0000

    Roughling working lambda with terraform infrastructure

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -103,6 +103,51 @@ resource "aws_s3_bucket" "prod_bucket" {
 EOF
 }
 
+resource "aws_iam_role" "iam_for_lambda" {
+    name = "iam_for_lambda"
+    assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_permission" "allow_bucket" {
+    statement_id = "AllowExecutionFromS3Bucket"
+    action = "lambda:InvokeFunction"
+    function_name = "${aws_lambda_function.func.arn}"
+    principal = "s3.amazonaws.com"
+    source_arn = "${aws_s3_bucket.prod_bucket.arn}"
+}
+
+resource "aws_lambda_function" "func" {
+    filename = "../resizer/.serverless/resizer.zip"
+    source_code_hash = "${base64sha256(file("../resizer/.serverless/resizer.zip"))}"
+    function_name = "resizer-lambda-2"
+    runtime = "nodejs4.3"
+    timeout = 30
+    memory_size = 512
+    role = "${aws_iam_role.iam_for_lambda.arn}"
+    handler = "handler.resize"
+}
+resource "aws_s3_bucket_notification" "bucket_notification" {
+    bucket = "${aws_s3_bucket.prod_bucket.id}"
+    lambda_function {
+        lambda_function_arn = "${aws_lambda_function.func.arn}"
+        events = ["s3:ObjectCreated:*"]
+        filter_suffix = ".jpg"
+    }
+}
+
 output "secret" {
   value = "${aws_iam_access_key.slate.secret}"
 
--- resizer/handler.js
+++ resizer/handler.js
@@ -1,16 +1,47 @@
-'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+var async = require('async');
+var AWS = require('aws-sdk');
+var gm = require('gm').subClass({"imageMagick" : true});
+var util = require('util');
 
-  callback(null, response);
+var s3 = new AWS.S3();
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+module.exports.resize = (event, context, callback) => {
+  "use strict";
+
+  var s3data = event.Records[0].s3;
+  async.waterfall([
+      function download(next) {
+        console.log("Starting to do download " + s3data.object.key);
+        s3.getObject({
+          Bucket: s3data.bucket.name,
+          Key: s3data.object.key
+        }, next);
+      },
+      function shrink(data, next) {
+        console.log("Starting to do shring... ");
+        console.log(util.inspect(data));
+        gm(data.Body)
+          .resize(100, 100)
+          .toBuffer('jpg', function (err, buffer) {
+            if (err) {
+              next(err);
+            } else {
+              next(null, data.ContentType, buffer);
+            }
+          });
+      },
+      function uploadToS3(contentType, data, next) {
+        console.log("Uploading back to S3");
+        s3.putObject({
+          Bucket: s3data.bucket.name,
+          Key: "images/small/"+s3data.object.key,
+          Body: data,
+          ContentType: contentType
+        }, next);
+      }],
+      function(err) {
+        console.log("Caught an error: " + err);
+        if(err) throw err;
+      });
 };
