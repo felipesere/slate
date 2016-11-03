6e050506de1df6965e5207915037c33c59f248c6
Felipe Sere <felipesere@gmail.com>
2016-11-03 10:26:47 +0000

    Get images from the new S3 folder

--- lib/templates/view.ex
+++ lib/templates/view.ex
@@ -4,6 +4,6 @@ defmodule View do
   def image(%{image: name}), do: image(name)
   def image({_, [image: name]}), do: image(name)
   def image(name) do
-    "#{Application.fetch_env!(:slate, :image_host)}/slate-inbox/#{name}"
+    "#{Application.fetch_env!(:slate, :image_host)}/slate-inbox/images/#{name}"
   end
 end
