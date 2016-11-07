957992d125b5558b2dc571464b016e1ffc47cabc
Felipe Sere <felipesere@gmail.com>
2016-11-07 23:19:42 +0000

    Its nice to see that the scss files were actually compiled

--- lib/mix/tasks/build_release.ex
+++ lib/mix/tasks/build_release.ex
@@ -2,6 +2,6 @@ defmodule Mix.Tasks.BuildRelease do
   use Mix.Task
 
   def run(_) do
-    System.cmd("sass", ["--sourcemap=none", "style/main.css.scss", "priv/static/main.css"])
+    System.cmd("sass", ["--sourcemap=none", "style/main.css.scss", "priv/static/main.css"], into: IO.stream(:stdio, :line))
   end
 end
