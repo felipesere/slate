159f0306a35ab1e4f4c265f1333397f5157e39a0
Felipe Sere <felipesere@gmail.com>
2016-11-22 13:27:32 +0100

    Test a single page

--- test/admin_test.exs
+++ test/admin_test.exs
@@ -8,7 +8,19 @@ defmodule AdminTests do
     assert body(conn) |> Floki.find(".asset-title") |> children() == ["London", "Madeira", "Spring", "Tenerife", "Waves", "Outcropping", "Beach", "Rocks"]
   end
 
+  test "single page" do
+    conn = get("/solo-image/6")
+
+    assert conn.status == 200
+    assert body(conn) |> Floki.find(".page-title") |> children() == ["Outcropping"]
+    assert body(conn) |> Floki.find(".asset-subtitle") |> children() == ["July 17, 2014"]
+    assert body(conn) |> Floki.find(".exif-data") |> children() |> present
+  end
+
 
   def children(elements) when is_list(elements), do: Enum.flat_map(elements, &children(&1))
   def children({_, _, x}), do: x
+
+  def present([]), do: false
+  def present(_elements), do: true
 end
