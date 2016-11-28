6013009296007a03387ab2ef2b6268eab4f4dd88
Felipe Sere <felipesere@gmail.com>
2016-11-28 21:21:39 +0000

    Move galleries query into the catalog

--- lib/slate/catalog.ex
+++ lib/slate/catalog.ex
@@ -1,4 +1,8 @@
 defmodule Slate.Catalog do
   use Ecto.Repo, otp_app: :slate
+  import Ecto.Query, only: [from: 2]
 
+  def gallery(id) do
+    one(from g in Gallery, where: g.id == ^id, preload: [:images])
+  end
 end
--- test/catalog_test.exs
+++ test/catalog_test.exs
@@ -1,6 +1,5 @@
 defmodule CatalogTests do
   use ExUnit.Case, async: true
-  import Ecto.Query, only: [from: 2]
   alias Slate.Catalog
 
 
@@ -18,7 +17,7 @@ defmodule CatalogTests do
 
     created_gallery = Catalog.insert!(%Gallery{title: "Some day", date: Ecto.Date.cast!(~D[2016-11-26]), images: [first, second]})
 
-    found = Catalog.one(from g in Gallery, where: g.id == ^created_gallery.id, preload: [:images])
+    found = Catalog.gallery(created_gallery.id)
     assert length(found.images) == 2
   end
 end
