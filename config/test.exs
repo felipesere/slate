5bdf1cc5d688078300ef81c5c3a867ea96ae75be
Felipe Sere <felipesere@gmail.com>
2016-12-02 08:55:03 +0000

    Create a separate DB for the test environment

--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,9 @@
 use Mix.Config
 
 config :slate, Slate.Catalog,
-  pool: Ecto.Adapters.SQL.Sandbox
+  pool: Ecto.Adapters.SQL.Sandbox,
+  database: "catalog_test",
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  port: "5432"
