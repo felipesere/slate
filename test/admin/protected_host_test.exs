438276ee95c2c1a3b69a0c99de9411d6174325ae
Felipe Sere <felipesere@gmail.com>
2016-12-02 22:40:41 +0000

    Plug redirects if the host does not match

--- test/admin/protected_host_test.exs
+++ test/admin/protected_host_test.exs
@@ -3,6 +3,24 @@ defmodule Slate.Admin.ProtectedHostTest do
   use Plug.Test
   alias Slate.Admin.ProtectedHost
 
+  test "pass-through for the right host" do
+    conn = conn(:get, "/", "") |> put_req_header("host", "really.secure.com")
+
+    conn = ProtectedHost.call(conn, host: "really.secure.com")
+
+    refute conn.halted
+  end
+
+  test "keeps any path params" do
+    conn = conn(:get, "/foo", "") |> put_req_header("host", "not.really.secure.com")
+
+    conn = ProtectedHost.call(conn, host: "really.secure.com")
+
+    assert conn.status == 301
+    assert Plug.Conn.get_resp_header(conn, "location") == ["really.secure.com/foo"]
+    assert conn.halted
+  end
+
   test "redirects if accessing on unsecure host" do
     conn = conn(:get, "/", "") |> put_req_header("host", "not.really.secure")
 
@@ -10,6 +28,6 @@ defmodule Slate.Admin.ProtectedHostTest do
 
     assert conn.halted
     assert conn.status == 301
-    assert Plug.Conn.get_resp_header(conn, "location") == ["really.secure.com"]
+    assert Plug.Conn.get_resp_header(conn, "location") == ["really.secure.com/"]
   end
 end
