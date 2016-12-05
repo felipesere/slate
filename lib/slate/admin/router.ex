08b78806fc00e2c1c6a416295f6bc133695470e6
Felipe Sere <felipesere@gmail.com>
2016-12-05 08:09:04 +0000

    Don't force the redirect to a protected host anywhere but in prod

--- lib/slate/admin/router.ex
+++ lib/slate/admin/router.ex
@@ -11,7 +11,9 @@ defmodule Slate.Admin.Router do
     put_in conn.secret_key_base, "2iSeS9fRD2JNnoIEWx5OvlGD1KNi5BUsdgB18CluqCnvCscIVvBh2LfEJyfWYtfs"
   end
 
-  plug Slate.Admin.ProtectedHost, host: "slate-blog.herokuapp.com", scheme: "https"
+  if Mix.env != :test do
+    plug Slate.Admin.ProtectedHost, host: "slate-blog.herokuapp.com", scheme: "https"
+  end
   plug Plug.Session,
     store: :cookie,
     key: "_hello_phoenix_key",
