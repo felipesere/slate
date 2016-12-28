3f2f01c196aa7ee7bd71e53ad0f3fb0b7bf02d12
Felipe Sere <felipesere@gmail.com>
2016-12-28 23:34:31 +0100

    Inline Plug.Conn

--- lib/slate/web.ex
+++ lib/slate/web.ex
@@ -1,18 +1,20 @@
 defmodule Web do
   defmacro __using__(_opts) do
     quote do
+      import Plug.Conn, only: [put_resp_header: 3, send_resp: 3, halt: 1]
+
       def redirect(conn, [to: target]) do
         conn
-        |> Plug.Conn.put_resp_header("location", target)
-        |> Plug.Conn.send_resp(302, "Redirecting...")
-        |> Plug.Conn.halt
+        |> put_resp_header("location", target)
+        |> send_resp(302, "Redirecting...")
+        |> halt
       end
 
       def not_found(conn) do
-        Plug.Conn.send_resp(conn, 404, "oops")
+        send_resp(conn, 404, "oops")
       end
 
-      defp respond(body, conn), do: Plug.Conn.send_resp(conn, 200, body)
+      defp respond(body, conn), do: send_resp(conn, 200, body)
     end
   end
 end
